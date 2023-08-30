49846931bcfe0d67bea648ad2229b76fc07cbb75
MrDave <davidschnoll@gmail.com>
2023-08-30 12:12:25 +0300

    Write script to remove chastisements

--- scripts.py
+++ scripts.py
@@ -1,7 +1,12 @@
-from datacenter.models import Schoolkid, Mark
+from datacenter.models import Schoolkid, Mark, Chastisement
 
 
 def fix_marks(schoolkid: Schoolkid):
     kid_marks = Mark.objects.filter(schoolkid=schoolkid)
     kid_bad_marks = kid_marks.filter(points__lte=3)
     kid_bad_marks.update(points=5)
+
+
+def remove_chastisements(schoolkid: Schoolkid):
+    kid_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    kid_chastisements.delete()
