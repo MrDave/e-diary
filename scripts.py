235b43c6d0da32460e1952dd0a4bd8a7aa140860
MrDave <davidschnoll@gmail.com>
2023-09-05 17:21:30 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,11 +1,25 @@
 from datacenter.models import Schoolkid, Mark, Chastisement, Subject, Lesson, Commendation
+from random import choice
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+
+TEXT_CHOICES = [
+            "Молодцом!",
+            "Хвалю!",
+            "Внимательно слушает учителя",
+            "Лучший ответ в классе!",
+            "Так держать!",
+            "Невероятный успех!",
+            "Лучше всех!",
+            "Пример для подражания",
+            "Гордость класса!",
+        ]
 
 
 def fix_marks(kid: str):
     schoolkid = fetch_kid(kid)
-    kid_marks = Mark.objects.filter(schoolkid=schoolkid)
-    kid_bad_marks = kid_marks.filter(points__lte=3)
-    kid_bad_marks.update(points=5)
+    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__lte=3)
+    bad_marks.update(points=5)
 
 
 def remove_chastisements(kid: str):
@@ -16,25 +30,34 @@ def remove_chastisements(kid: str):
 
 def create_commendation(kid: str, subject_name: str):
     schoolkid = fetch_kid(kid)
-    subject = Subject.objects.get(
-        title=subject_name,
-        year_of_study=schoolkid.year_of_study
-    )
-    lesson = Lesson.objects.filter(
-        year_of_study=schoolkid.year_of_study,
-        group_letter=schoolkid.group_letter,
-        subject=subject
-    ).order_by("-date").first()
+    try:
+        subject = Subject.objects.get(
+            title=subject_name,
+            year_of_study=schoolkid.year_of_study
+        )
 
-    Commendation.objects.create(
-        text="Молодцом!",
-        created=lesson.date,
-        schoolkid=schoolkid,
-        subject=subject,
-        teacher=lesson.teacher,
-    )
+        lesson = Lesson.objects.filter(
+            year_of_study=schoolkid.year_of_study,
+            group_letter=schoolkid.group_letter,
+            subject=subject
+        ).order_by("-date").first()
+
+        Commendation.objects.create(
+            text=choice(TEXT_CHOICES),
+            created=lesson.date,
+            schoolkid=schoolkid,
+            subject=subject,
+            teacher=lesson.teacher,
+        )
+    except Subject.DoesNotExist:
+        raise ObjectDoesNotExist("Нет такого предмета")
 
 
 def fetch_kid(kid_name: str):
-    school_kid = Schoolkid.objects.get(full_name__contains=kid_name)
-    return school_kid
+    try:
+        kid = Schoolkid.objects.get(full_name__contains=kid_name)
+        return kid
+    except Schoolkid.DoesNotExist:
+        raise ObjectDoesNotExist("Такого имени не существует")
+    except Schoolkid.MultipleObjectsReturned:
+        raise MultipleObjectsReturned("Найдено больше 1 совпадения")
